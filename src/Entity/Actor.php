29e4e92585570df0b7403ff28314c82cf0f32d12
Abecker37 <72383676+Abecker37@users.noreply.github.com>
2023-05-31 15:56:21 +0200

    quete22

--- src/Entity/Actor.php
+++ src/Entity/Actor.php
@@ -2,12 +2,19 @@
 
 namespace App\Entity;
 
-use App\Repository\ActorRepository;
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
+use DateTime;
+use DateTimeInterface;
+use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use App\Repository\ActorRepository;
+use Doctrine\Common\Collections\Collection;
+use Symfony\Component\HttpFoundation\File\File;
+use Doctrine\Common\Collections\ArrayCollection;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: ActorRepository::class)]
+#[Vich\Uploadable]
 class Actor
 {
     #[ORM\Id]
@@ -21,6 +28,18 @@ class Actor
     #[ORM\ManyToMany(targetEntity: Program::class, inversedBy: 'actors')]
     private Collection $programs;
 
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $poster = null;
+    
+    #[Vich\UploadableField(mapping: 'actor_file', fileNameProperty: 'poster')]
+    #[Assert\File(
+    maxSize: '1M',
+    mimeTypes: ['image/jpeg', 'image/png', 'image/webp'],)]
+    private ?File $actorFile = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE, nullable: true)]
+    private ?DatetimeInterface $updatedAt = null;
+    
     public function __construct()
     {
         $this->programs = new ArrayCollection();
@@ -64,6 +83,69 @@ class Actor
     {
         $this->programs->removeElement($program);
 
+        return $this;
+    }
+
+    /**
+     * Get the value of updatedAt
+     */ 
+    public function getUpdatedAt()
+    {
+        return $this->updatedAt;
+    }
+
+    /**
+     * Set the value of updatedAt
+     *
+     * @return  self
+     */ 
+    public function setUpdatedAt($updatedAt)
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of poster
+     */ 
+    public function getPoster()
+    {
+        return $this->poster;
+    }
+
+    /**
+     * Set the value of poster
+     *
+     * @return  self
+     */ 
+    public function setPoster($poster)
+    {
+        $this->poster = $poster;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of actorFile
+     */ 
+    public function getActorFile()
+    {
+        return $this->actorFile;
+    }
+
+    /**
+     * Set the value of actorFile
+     *
+     * @return  self
+     */ 
+    public function setActorFile(File $image)
+    {
+        $this->actorFile = $image;
+        if ($image) {
+
+            $this->updatedAt = new DateTime('now');
+        }
         return $this;
     }
 }
